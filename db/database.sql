aeb59eee625379e6e98b4c48ab669904a01cdd29
Lucas Oliveira <lima1301lucas@gmail.com>
2025-10-05 12:29:01 -0300

    inclusão da tabela de usuarios no BD

--- db/database.sql
+++ db/database.sql
@@ -1,6 +1,16 @@
 CREATE DATABASE todo_app;
 USE todo_app;
 
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    nome VARCHAR(50) NOT NULL,
+    sobrenome VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    inativo TINYINT(1) DEFAULT 0
+);
+
 CREATE TABLE prioridades(
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(20) NOT NULL
